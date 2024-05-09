14eeec23e0fc21151a95a106e2ac6f0f6b9f1d09
sysradium <sysradium@users.noreply.github.com>
2024-05-10 00:53:57 +0200

    move things around

--- users-service/internal/outbox/basic/basic.go
+++ users-service/internal/outbox/basic/basic.go
@@ -2,16 +2,12 @@ package basic
 
 import (
 	"context"
-	"fmt"
 	"log/slog"
-	"time"
 
-	"github.com/ThreeDotsLabs/watermill/message"
 	"github.com/sysradium/debezium-outbox-example/users-service/internal/outbox"
 	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
 )
 
 type Outbox struct {
@@ -24,93 +20,6 @@ type Outbox struct {
 
 type Marshaler interface {
 	Marshal(proto.Message) ([]byte, error)
-}
-
-type Worker struct {
-	db              *gorm.DB
-	logger          *slog.Logger
-	marshaler       Marshaler
-	pollingInterval time.Duration
-	publisher       message.Publisher
-	batchSize       int
-	topicPrefix     string
-	done            <-chan struct{}
-	ctx             context.Context
-	cancel          context.CancelFunc
-}
-
-func NewWorker(db *gorm.DB, publisher message.Publisher, opts ...workerOption) *Worker {
-	ctx, cancel := context.WithCancel(context.Background())
-	w := &Worker{
-		db:     db,
-		logger: slog.Default(),
-		marshaler: protojson.MarshalOptions{
-			UseProtoNames: true,
-			Multiline:     false,
-		},
-		publisher: publisher,
-		batchSize: 10000,
-		ctx:       ctx,
-		cancel:    cancel,
-	}
-
-	for _, o := range opts {
-		o(w)
-	}
-
-	return w
-}
-
-func (p *Worker) Start() {
-	ticker := time.NewTicker(p.pollingInterval)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-p.ctx.Done():
-			break
-		case <-ticker.C:
-			p.processMessages(p.ctx, p.batchSize)
-		}
-	}
-}
-
-func (p *Worker) Stop() {
-	p.cancel()
-	<-p.done
-}
-
-func (p *Worker) processMessages(ctx context.Context, batchSize int) error {
-	db := p.db.WithContext(ctx).Begin()
-	defer db.Commit()
-
-	var messages []Outbox
-	res := db.Limit(batchSize).Clauses(
-		clause.Locking{
-			Strength: clause.LockingStrengthUpdate,
-			Options:  clause.LockingOptionsSkipLocked,
-		},
-	).Find(&messages)
-	if res.Error != nil {
-		return res.Error
-	}
-
-	if len(messages) == 0 {
-		return nil
-	}
-
-	for _, m := range messages {
-		if err := p.publisher.Publish(fmt.Sprintf("outbox.event.%v", m.AggregateType), message.NewMessage(m.ID, m.Payload)); err != nil {
-			p.logger.Error("unable to publish message", "error", err)
-		}
-	}
-
-	if res := db.Delete(&messages); res.Error != nil {
-		p.logger.Error("unable to delete message batch")
-	}
-
-	return nil
-
 }
 
 type OutboxStorage struct {
---
+++ users-service/internal/outbox/basic/worker.go
@@ -0,0 +1,106 @@
+package basic
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"time"
+
+	"github.com/ThreeDotsLabs/watermill/message"
+	"google.golang.org/protobuf/encoding/protojson"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+type Worker struct {
+	db              *gorm.DB
+	logger          *slog.Logger
+	marshaler       Marshaler
+	pollingInterval time.Duration
+	publisher       message.Publisher
+	batchSize       int
+	topicPrefix     string
+	done            chan struct{}
+	ctx             context.Context
+	cancel          context.CancelFunc
+}
+
+func NewWorker(db *gorm.DB, publisher message.Publisher, opts ...workerOption) *Worker {
+	ctx, cancel := context.WithCancel(context.Background())
+	w := &Worker{
+		db:     db,
+		logger: slog.Default(),
+		marshaler: protojson.MarshalOptions{
+			UseProtoNames: true,
+			Multiline:     false,
+		},
+		publisher: publisher,
+		batchSize: 10000,
+		ctx:       ctx,
+		cancel:    cancel,
+	}
+
+	for _, o := range opts {
+		o(w)
+	}
+
+	return w
+}
+
+func (p *Worker) Start() {
+	ticker := time.NewTicker(p.pollingInterval)
+	defer func() {
+		ticker.Stop()
+		p.done <- struct{}{}
+	}()
+
+	for {
+		select {
+		case <-p.ctx.Done():
+			return
+		case <-ticker.C:
+			p.processMessages(p.ctx, p.batchSize)
+		}
+	}
+
+}
+
+func (p *Worker) Stop() {
+	p.cancel()
+	<-p.done
+}
+
+func (p *Worker) processMessages(ctx context.Context, batchSize int) error {
+	db := p.db.WithContext(ctx).Begin()
+	defer db.Commit()
+
+	var messages []Outbox
+	res := db.Limit(batchSize).Clauses(
+		clause.Locking{
+			Strength: clause.LockingStrengthUpdate,
+			Options:  clause.LockingOptionsSkipLocked,
+		},
+	).Find(&messages)
+	if res.Error != nil {
+		return res.Error
+	}
+
+	if len(messages) == 0 {
+		return nil
+	}
+
+	for _, m := range messages {
+		if err := p.publisher.Publish(
+			fmt.Sprintf("%v.event.%v", p.topicPrefix, m.AggregateType),
+			message.NewMessage(m.ID, m.Payload),
+		); err != nil {
+			p.logger.Error("unable to publish message", "error", err)
+		}
+	}
+
+	if res := db.Delete(&messages); res.Error != nil {
+		p.logger.Error("unable to delete message batch")
+	}
+
+	return nil
+}
